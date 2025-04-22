a935f32b347bf5891590fe907cdb169c380a1833
CHANDRAPRABHA-03 <vchandraprabha903@gmail.com>
2025-04-23 01:25:25 +0530

    pushing all the code to git

---
+++ JS/Day1.js
@@ -0,0 +1,12 @@
+let name = "Bob";
+console.log(typeof name); // string
+
+let name1 = "Ravi";
+let age = 22;
+let isStudent = true;
+let hobbies = ["Reading", "Coding"];
+
+console.log(typeof name1);     // string
+console.log(typeof age);      // number
+console.log(typeof isStudent); // boolean
+console.log(typeof hobbies);  // object
